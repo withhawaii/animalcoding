f0c1fdd6fe8589ceb74a2773ef2a477833ec1f04
Genta T <genta@hawaii.edu>
2025-05-20 14:32:03 -1000

    Fix NES button

--- js/player.js
+++ js/player.js
@@ -69,7 +69,7 @@ class Player extends Phaser.GameObjects.Sprite {
 
   error() {
     this.hangUp(100);
-    this.updateEnergy(-1 * currentPlayer.energy);  
+    this.updateEnergy(-1 * this.energy);  
   }
 
   turn(step) {
