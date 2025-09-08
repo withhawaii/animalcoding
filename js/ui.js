06bd834937c5ac1836e33a8750fe265267f539be
Genta T <genta@hawaii.edu>
2025-09-08 07:07:45 -1000

    Rename game.js to ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,79 +1,79 @@
-const editor_config = {
-  selectionStyle: 'line',// 'line'|'text'
-  highlightActiveLine: true, // boolean
-  highlightSelectedWord: true, // boolean
-  readOnly: false, // boolean: true if read only
-  cursorStyle: 'ace', // 'ace'|'slim'|'smooth'|'wide'
-  mergeUndoDeltas: true, // false|true|'always'
-  behavioursEnabled: true, // boolean: true if enable custom behaviours
-  wrapBehavioursEnabled: true, // boolean
-  autoScrollEditorIntoView: undefined, // boolean: this is needed if editor is inside scrollable page
-  keyboardHandler: null, // function: handle custom keyboard events
-  
-  // renderer options
-  animatedScroll: false, // boolean: true if scroll should be animated
-  displayIndentGuides: false, // boolean: true if the indent should be shown. See 'showInvisibles'
-  showInvisibles: true, // boolean -> displayIndentGuides: true if show the invisible tabs/spaces in indents
-  showPrintMargin: true, // boolean: true if show the vertical print margin
-  printMarginColumn: 80, // number: number of columns for vertical print margin
-  printMargin: undefined, // boolean | number: showPrintMargin | printMarginColumn
-  showGutter: true, // boolean: true if show line gutter
-  fadeFoldWidgets: false, // boolean: true if the fold lines should be faded
-  showFoldWidgets: true, // boolean: true if the fold lines should be shown ?
-  showLineNumbers: true,
-  highlightGutterLine: false, // boolean: true if the gutter line should be highlighted
-  hScrollBarAlwaysVisible: false, // boolean: true if the horizontal scroll bar should be shown regardless
-  vScrollBarAlwaysVisible: false, // boolean: true if the vertical scroll bar should be shown regardless
-  fontSize: 14, // number | string: set the font size to this many pixels
-  fontFamily: undefined, // string: set the font-family css value
-  maxLines: undefined, // number: set the maximum lines possible. This will make the editor height changes
-  minLines: undefined, // number: set the minimum lines possible. This will make the editor height changes
-  maxPixelHeight: 0, // number -> maxLines: set the maximum height in pixel, when 'maxLines' is defined. 
-  scrollPastEnd: 0, // number -> !maxLines: if positive, user can scroll pass the last line and go n * editorHeight more distance 
-  fixedWidthGutter: false, // boolean: true if the gutter should be fixed width
-
-  // mouseHandler options
-  scrollSpeed: 2, // number: the scroll speed index
-  dragDelay: 0, // number: the drag delay before drag starts. it's 150ms for mac by default 
-  dragEnabled: true, // boolean: enable dragging
-  tooltipFollowsMouse: true, // boolean: true if the gutter tooltip should follow mouse
-
-  // session options
-  firstLineNumber: 1, // number: the line number in first line
-  overwrite: false, // boolean
-  newLineMode: 'auto', // 'auto' | 'unix' | 'windows'
-  useWorker: true, // boolean: true if use web worker for loading scripts
-  useSoftTabs: true, // boolean: true if we want to use spaces than tabs
-  tabSize: 2, // number
-  wrap: false, // boolean | string | number: true/'free' means wrap instead of horizontal scroll, false/'off' means horizontal scroll instead of wrap, and number means number of column before wrap. -1 means wrap at print margin
-  indentedSoftWrap: false, // boolean
-  foldStyle: 'markbegin', // enum: 'manual'/'markbegin'/'markbeginend'.
-  theme: 'ace/theme/monokai',
-  mode: 'ace/mode/javascript' // string: path to language mode 
-}
-
-const game_config = {
-  type: Phaser.AUTO,
-  backgroundColor: '#7DB8EF',
-  parent: 'game-container',
-  width: 1024,
-  height: 704,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 0 }
-    }
-  },
-  scene: [BootScene, TitleScene, MainScene, ResultScene]
-};
-
 const ui = {
   editor: null,
   interpreter: null,
   game: null,
   currentPlayer: null,
 
-  initInterpreter(interpreter, scope) {
+  editorConfig: {
+    selectionStyle: 'line',// 'line'|'text'
+    highlightActiveLine: true, // boolean
+    highlightSelectedWord: true, // boolean
+    readOnly: false, // boolean: true if read only
+    cursorStyle: 'ace', // 'ace'|'slim'|'smooth'|'wide'
+    mergeUndoDeltas: true, // false|true|'always'
+    behavioursEnabled: true, // boolean: true if enable custom behaviours
+    wrapBehavioursEnabled: true, // boolean
+    autoScrollEditorIntoView: undefined, // boolean: this is needed if editor is inside scrollable page
+    keyboardHandler: null, // function: handle custom keyboard events
+    
+    // renderer options
+    animatedScroll: false, // boolean: true if scroll should be animated
+    displayIndentGuides: false, // boolean: true if the indent should be shown. See 'showInvisibles'
+    showInvisibles: true, // boolean -> displayIndentGuides: true if show the invisible tabs/spaces in indents
+    showPrintMargin: true, // boolean: true if show the vertical print margin
+    printMarginColumn: 80, // number: number of columns for vertical print margin
+    printMargin: undefined, // boolean | number: showPrintMargin | printMarginColumn
+    showGutter: true, // boolean: true if show line gutter
+    fadeFoldWidgets: false, // boolean: true if the fold lines should be faded
+    showFoldWidgets: true, // boolean: true if the fold lines should be shown ?
+    showLineNumbers: true,
+    highlightGutterLine: false, // boolean: true if the gutter line should be highlighted
+    hScrollBarAlwaysVisible: false, // boolean: true if the horizontal scroll bar should be shown regardless
+    vScrollBarAlwaysVisible: false, // boolean: true if the vertical scroll bar should be shown regardless
+    fontSize: 14, // number | string: set the font size to this many pixels
+    fontFamily: undefined, // string: set the font-family css value
+    maxLines: undefined, // number: set the maximum lines possible. This will make the editor height changes
+    minLines: undefined, // number: set the minimum lines possible. This will make the editor height changes
+    maxPixelHeight: 0, // number -> maxLines: set the maximum height in pixel, when 'maxLines' is defined. 
+    scrollPastEnd: 0, // number -> !maxLines: if positive, user can scroll pass the last line and go n * editorHeight more distance 
+    fixedWidthGutter: false, // boolean: true if the gutter should be fixed width
+
+    // mouseHandler options
+    scrollSpeed: 2, // number: the scroll speed index
+    dragDelay: 0, // number: the drag delay before drag starts. it's 150ms for mac by default 
+    dragEnabled: true, // boolean: enable dragging
+    tooltipFollowsMouse: true, // boolean: true if the gutter tooltip should follow mouse
+
+    // session options
+    firstLineNumber: 1, // number: the line number in first line
+    overwrite: false, // boolean
+    newLineMode: 'auto', // 'auto' | 'unix' | 'windows'
+    useWorker: true, // boolean: true if use web worker for loading scripts
+    useSoftTabs: true, // boolean: true if we want to use spaces than tabs
+    tabSize: 2, // number
+    wrap: false, // boolean | string | number: true/'free' means wrap instead of horizontal scroll, false/'off' means horizontal scroll instead of wrap, and number means number of column before wrap. -1 means wrap at print margin
+    indentedSoftWrap: false, // boolean
+    foldStyle: 'markbegin', // enum: 'manual'/'markbegin'/'markbeginend'.
+    theme: 'ace/theme/monokai',
+    mode: 'ace/mode/javascript' // string: path to language mode 
+  },
+
+  gameConfig: {
+    type: Phaser.AUTO,
+    backgroundColor: '#7DB8EF',
+    parent: 'game-container',
+    width: 1024,
+    height: 704,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 0 }
+      }
+    },
+    scene: [BootScene, TitleScene, MainScene, ResultScene]
+  },
+
+  interpreterConfig(interpreter, scope) {
     interpreter.setProperty(scope, 'move_forward', interpreter.createNativeFunction(function() {
       ui.log('move_forward');
       return ui.currentPlayer.move();
@@ -97,7 +97,7 @@ const ui = {
     ui.disableButton('skip');
     ui.currentPlayer.stopIdle();
     try {
-      ui.interpreter = new Interpreter(ui.editor.getValue(), ui.initInterpreter);
+      ui.interpreter = new Interpreter(ui.editor.getValue(), ui.interpreterConfig);
       setTimeout(ui.runStep, 110);
     }
     catch(e) {
@@ -125,20 +125,20 @@ const ui = {
     } 
   },
 
-  skipTurn() {
-    ui.disableButton('run_code');
-    ui.disableButton('skip');
-    ui.currentPlayer.stopIdle();
-    ui.interpreter = new Interpreter('', ui.initInterpreter);
-    setTimeout(runCode, 110);
-  },
-
   handleError(error) {
     document.getElementById('error-message').innerHTML = error.message;
     document.getElementById('dialog-default').showModal();
     ui.log('Error:', ui.currentPlayer, error);
     ui.currentPlayer.fail();
     ui.currentPlayer.scene.changePlayer();  
+  },
+
+  skipTurn() {
+    ui.disableButton('run_code');
+    ui.disableButton('skip');
+    ui.currentPlayer.stopIdle();
+    ui.interpreter = new Interpreter('', ui.initInterpreter);
+    setTimeout(runCode, 110);
   },
 
   loadSnippets(key) {
@@ -265,9 +265,8 @@ const ui = {
       }
     });
 
-    ui.editor = ace.edit('editor', editor_config);
-//    ui.editor.setValue('/*\nAvailable commands:\nturn_right();\nturn_left();\nmove_forward();\npick_up();\n*/\n')
-    ui.game = new Phaser.Game(game_config);
+    ui.editor = ace.edit('editor', ui.editorConfig);
+    ui.game = new Phaser.Game(ui.gameConfig);
     ui.disableButton('run_code');
     ui.disableButton('skip');
   }
